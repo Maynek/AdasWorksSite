dd82bf5f599505a3ee850777759999c0f7d38f0a
Ada Maynek <ada.maynek@gmail.com>
2024-07-29 00:27:07 +0900

    no message

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -8,7 +8,7 @@ export default function About() {
   return (
     <Common>
       <h1 className="font-bold text-3xl mb-4">このサイトについて</h1>
-      <p>Ada Maynek が作成している Web 小説「 Programmable stars ~ 俺のプログラミングであの子を最強化するまでの物語 ~ 」を公開するためのサイトです。</p>
+      <p>Ada Maynek が執筆している Web 小説 を公開するためのサイトです。</p>
     </Common>
   );
 }
